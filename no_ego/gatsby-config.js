b7969579955db8e6d27ee0791fe7f7e0d94a0689
Andrew <developer@no-ego.net>
2020-04-09 12:46:45 -0400

    Add accessibility and SEO features
    Using lighthouse to audit site

--- no_ego/gatsby-config.js
+++ no_ego/gatsby-config.js
@@ -1,8 +1,26 @@
 module.exports = {
   siteMetadata: {
-    title: `nO fuiGO`,
+    title: `nO EgO`,
+    description: `A simple site to explore things without the ego demanding perfection decimate goodness`,
+    author: `developer of no-ego.net`,
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `no-ego.net`,
+        short_name: `no ego : humility is a virtue`,
+        start_url: `/`,
+        background_color: `#6b37bf`,
+        theme_color: `#6b37bf`,
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: `standalone`,
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
